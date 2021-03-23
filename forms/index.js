eb622c17bbb5ea9752718d73ef01d2eafa7f74e4
yongsannnn <yong_s@live.com>
2021-03-23 07:28:06 +0000

    Completed Create Poster

---
+++ forms/index.js
@@ -0,0 +1,87 @@
+const forms = require("forms")
+
+const fields = forms.fields;
+const validators = forms.validators;
+
+
+var bootstrapField = function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var validationclass = object.value && !object.error ? 'is-valid' : '';
+    validationclass = object.error ? 'is-invalid' : validationclass;
+    if (validationclass) {
+        object.widget.classes.push(validationclass);
+    }
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="invalid-feedback">' + object.error + '</div>' : '';
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group">' + label + widget + error + '</div>';
+};
+
+
+const createPosterForm = () => {
+    return forms.create({
+        "title": fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClass:{
+                label: ["form-label"]
+            }
+        }),
+        "cost": fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClass:{
+                label: ["form-label"]
+            },
+            validators:[validators.integer()]
+        }),
+        "description": fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClass:{
+                label: ["form-label"]
+            }
+        }),
+        "date": fields.date({
+            required: true,
+            errorAfterField: true,
+            cssClass:{
+                label: ["form-label"]
+            }
+        }),
+        "stock": fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClass:{
+                label: ["form-label"]
+            },
+            validators:[validators.integer()]
+        }),
+        "height": fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClass:{
+                label: ["form-label"]
+            },
+            validators:[validators.integer()]
+        }),
+        "width": fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClass:{
+                label: ["form-label"]
+            },
+            validators:[validators.integer()]
+        })
+    })
+}
+
+
+module.exports={createPosterForm,bootstrapField}
--- routes/posters.js
+++ routes/posters.js
@@ -6,10 +6,44 @@ const router = express.Router()
 const {Poster} = require("../models")
 
 // Import forms
+const {createPosterForm, bootstrapField}=require("../forms")
+
 
 router.get("/", async(req,res)=>{
     let posters = await Poster.collection().fetch();
-    res.send(posters)
+    res.render("posters/index",{
+        "posters": posters.toJSON()
+    })
+})
+
+router.get("/create", (req,res)=>{
+    const posterForm = createPosterForm();
+    res.render("posters/create",{
+        "form": posterForm.toHTML(bootstrapField)
+    })
+})
+
+router.post("/create", (req,res)=>{
+    const posterForm = createPosterForm();
+    posterForm.handle(req,{
+        "success": async(form)=>{
+            const newPoster = new Poster();
+            newPoster.set("title", form.data.title)
+            newPoster.set("cost", form.data.cost)
+            newPoster.set("description", form.data.description)
+            newPoster.set("date", form.data.date)
+            newPoster.set("stock", form.data.stock)
+            newPoster.set("height", form.data.height)
+            newPoster.set("width", form.data.width)
+            await newPoster.save();
+            res.redirect("/posters")
+        },
+        "error":(form)=>{
+            res.render("posters/create",{
+                "form": form.toHTML(bootstrapField)
+            })
+        }
+    })
 })
 
 
